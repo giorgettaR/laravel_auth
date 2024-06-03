64763469529f1182913b276861f65639c161b8d9
GiorgettaR <giorgetta.roberto@gmail.com>
2024-06-03 16:18:09 +0200

    set up authentication routes / creation basic structure for the resource: projects

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+
+        Route::resource('projects', ProjectController::class);
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
